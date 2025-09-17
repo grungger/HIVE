be208892937ae881c63899370e11d8d2f66f2bce
Gian Rungger <grungger@student.ethz.ch>
2025-09-17 22:00:30 +0400

    Display test is working

--- derived_components/tests/display_test.cpp
+++ derived_components/tests/display_test.cpp
@@ -7,18 +7,36 @@
 #include "word_switch_in.hpp"
 #include <sstream>
 #include <iostream>
+#include <iomanip>
+#include <string>
+#include <cctype>
 
 using namespace Virtual;
 
+void print_as_raw(const std::string& s) {
+    for (unsigned char c : s) {
+        if (std::isprint(c)) {
+            std::cout << c;              // printable as-is
+        } else {
+            std::cout << "\\x"             // escape non-printables
+                      << std::setw(2)
+                      << std::setfill('0')
+                      << std::hex
+                      << static_cast<int>(c);
+        }
+    }
+    std::cout << std::dec; // reset formatting
+}  
+
 int main() {
 
-std::stringstream buffer;
+std::ostringstream buffer;
 std::streambuf* old = std::cout.rdbuf(buffer.rdbuf());
 Switch_in save(false);
 Switch_in flush(true);
 WordSwitch_in input(11);
 
-Display Dis( save.release_output(0), flush.release_output(0), input.release_word_output(0), 5.0);
+Display Dis( save.release_output(0), flush.release_output(0), input.release_word_output(0), 0.01);
 
 Dis.compute_output(); // expected: escape codes start
 save.change_state(true);
@@ -44,17 +62,21 @@ Dis.compute_output(); // expected: displayed memory
 std::cout.rdbuf(old);
 
 // Prediction:
-std::string expectedOutput = u8"\x1b[?1049h\x1b[?25l11 11 77 11 77 0 3000 2999 222 ";
-expectedOutput += "█▀▄█  ▄                         \n\
-   ▄▄▄ ▄▄▄ ▄                    \n\
-▀██▄█▀▄█▀▀ ▀                    \n";
+std::string expectedOutput = R"(\x1b[?1049h\x1b[?25l\x1b[H11 \x0a11 77 \x0a11 77 0 3000 2999 222 \x0a)";
+expectedOutput += R"(\x1b[H\xe2\x96\x88\xe2\x96\x80\xe2\x96\x84\xe2\x96\x88  \xe2\x96\x84                         \x0a   \xe2\x96\x84\xe2\x96\x84\xe2\x96\x84 \xe2\x96\x84\xe2\x96\x84\xe2\x96\x84 \xe2\x96\x84                    \x0a\xe2\x96\x80\xe2\x96\x88\xe2\x96\x88\xe2\x96\x84\xe2\x96\x88\xe2\x96\x80\xe2\x96\x84\xe2\x96\x88\xe2\x96\x80\xe2\x96\x80 \xe2\x96\x80                    \x0a)";
 
 std::string capturedOutput = buffer.str();
-
+std::ostringstream buffer2;
+std::streambuf* old2 = std::cout.rdbuf(buffer2.rdbuf());
+print_as_raw(capturedOutput);
+std::cout.rdbuf(old2);
+capturedOutput = buffer2.str();
 
 if (capturedOutput != expectedOutput) {
   std::cerr << "Expected: " << expectedOutput << std::endl;
   std::cerr << "Output: " << capturedOutput << std::endl;
+  // print_as_raw(capturedOutput);
+  std::cout << std::endl;
   throw std::logic_error("Test failed!");
 }
 
